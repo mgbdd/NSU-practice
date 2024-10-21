7aa86dd7879bc98a7b6342acae2217ba47d05c90
mgbdd <132687128+mgbdd@users.noreply.github.com>
2024-10-21 15:50:24 +0700

    Update data_reader.py

--- data_reader.py
+++ data_reader.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 def read_iqdat_file(file_path):
-    """
-    Функция для чтения бинарного файла в формате .iqdat и возвращения DataFrame.
-    """
     with open(file_path, 'r') as f:
         content = f.read()
         # Читаем заголовок файла
@@ -13,9 +10,11 @@ def read_iqdat_file(file_path):
 
     return df
 
+def main():
+    df1 = read_iqdat_file('Som_T_001_v1.iqdat')
+    st.dataframe(df1)
+    df2 = pd.read_csv("Som_T_001_v1_Face.csv", sep='\t')
+    st.dataframe(df2)
 
-df1 = read_iqdat_file('Som_T_001_v1.iqdat')
-st.dataframe(df1)
-
-df2 = pd.read_csv("Som_T_001_v1_Face.csv", sep='\t')
-st.dataframe(df2)
+if __name__ == "__main__":
+    main()
